0fdf1c0578cd60346d59d646c72dfba5654179d2
Syed Namoos Ali Shah <namoosshah@gmail.com>
2020-12-11 01:12:53 +0500

    html id issue fixed

--- src/Http/Controllers/EventsController.php
+++ src/Http/Controllers/EventsController.php
@@ -44,7 +44,7 @@ class EventsController
         $range = $start . ' - ' . $end;
         $html = '<div class="text-sm"><div class="flex justify-between">'.
             '<div>'.$event->title.'</div>'.
-            '<a class="editEvent"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="edit" role="presentation" class="fill-current"><path d="M4.3 10.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM6 14h2.59l9-9L15 2.41l-9 9V14zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h6a1 1 0 1 1 0 2H2v14h14v-6z"></path></svg></a>'.
+            '<a id="editEvent'.$event->id.'"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="edit" role="presentation" class="fill-current"><path d="M4.3 10.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM6 14h2.59l9-9L15 2.41l-9 9V14zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h6a1 1 0 1 1 0 2H2v14h14v-6z"></path></svg></a>'.
             '</div>'.
             '<div>Doctor: '.$doctor .'</div>'.
             '<div>Patient: '.$patient .'</div>'.
